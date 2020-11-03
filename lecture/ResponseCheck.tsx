9ef1a70c67a80c1491a3014245cf71e4b5c79bb6
space-between <xodyd1756@naver.com>
2020-11-03 23:18:56 +0900

    그냥

--- lecture/ResponseCheck.tsx
+++ lecture/ResponseCheck.tsx
@@ -4,14 +4,14 @@ import { useRef, useState, useCallback } from "react";
 const ResponseCheck = () => {
   const [state, setState] = useState("waiting");
   const [message, setMessage] = useState("클릭해서 시작하세요.");
-  const [result, setResult] = useState<number[]>([]);
-  const timeout = useRef<number>(null);
+  const [result, setResult] = useState<number[]>([]); //스테이트가 처음에 빈 배열일때에는 타입추론이 안될 수 있어서 타입을 제네릭으로 지정해줘야 한다.
+  const timeout = useRef<number | null>(null);
   const startTime = useRef(0);
   const endTime = useRef(0);
 
   const onClickScreen = useCallback(() => {
     if (state === 'waiting') {
-      timeout.current = window.setTimeout(() => {
+      timeout.current = window.setTimeout(() => { //타입스크립트에서 setTimeout을 사용할때는 브라우저인지 노드인지 구분해줘야한다. 지금은 브라우저이기 때문에 window를 앞에 붙이면 된다.
         setState('now');
         setMessage('지금 클릭');
         startTime.current = new Date().getTime();
--- lecture/client.tsx
+++ lecture/client.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-import ResponseCheck from './ResponseCheck'; //export default가 있는 파일은 불러올때 * as 이거 안 붙여도 된다.
+import RSP from './RSP'; //export default가 있는 파일은 불러올때 * as 이거 안 붙여도 된다.
 
-const Hot = hot(ResponseCheck); //이걸 하이오더컴포넌트라고 한다. 줄여서 HOC라고 부른다. 컴포넌트를 다른 컴포넌트로 감싸는 역할을 하고 있다. 이것을 해야지만 react핫로더가 적용된다.
+const Hot = hot(RSP); //이걸 하이오더컴포넌트라고 한다. 줄여서 HOC라고 부른다. 컴포넌트를 다른 컴포넌트로 감싸는 역할을 하고 있다. 이것을 해야지만 react핫로더가 적용된다.
 
 ReactDOM.render(<Hot />, document.querySelector('#root'));
